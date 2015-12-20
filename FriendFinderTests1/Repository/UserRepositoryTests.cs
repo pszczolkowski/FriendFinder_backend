e0f1f93ee4a362da43838e266abbe19a459b06da
Katarzyna <kaj915@wp.pl>
2015-12-20 23:03:00 +0100

    tests for UserRepository

--- FriendFinderTests1/Repository/UserRepositoryTests.cs
+++ FriendFinderTests1/Repository/UserRepositoryTests.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using FriendFinder.Models;
-
-
+using FriendFinder.ViewModels;
+using Microsoft.AspNet.Identity.EntityFramework;
 using FriendFinder.Repository;
 using System;
 using System.Collections.Generic;
@@ -12,64 +12,124 @@ namespace FriendFinder.Tests
     [TestClass()]
     public class UserRepositoryTests
     {
-        private static Mock<UserRepository> userRepoMock = new Mock<UserRepository>();
+        public static Mock<UserRepository> userRepoMock = new Mock<UserRepository>();
 
         [TestMethod()]
         public void FindByIdTest()
         {
-            var appUser = new ApplicationUser();
-            userRepoMock.Setup(x => x.FindById("239f4fae-ff76-4c80-b86c-b56666f4ac2e"));
+            var appUser = new ApplicationUser()
+            {
+                UserName = "friend",
+                Id = "239f4fae-ff76-4c80-b86c-b56666f4ac2e",
+                Position = new Position() { Longitude = 16.9, Latitude = 54.6, LastUpdate = DateTime.Now },
+
+            };
+            userRepoMock.Setup(x => x.FindById("239f4fae-ff76-4c80-b86c-b56666f4ac2e")).Returns(appUser);
             var user = userRepoMock.Object.FindById("239f4fae-ff76-4c80-b86c-b56666f4ac2e");
-            Assert.IsNull(user);
+            Assert.IsNotNull(user);
         }
 
         [TestMethod()]
         public void FindAllTest()
         {
             List<ApplicationUser> users = new List<ApplicationUser>();
-            var user = new ApplicationUser();
-            var user2 = new ApplicationUser();
-            var user3 = new ApplicationUser();
-            users.Add(user);
-            users.Add(user2);
-            users.Add(user3);
+            users.Add(new ApplicationUser()
+            {
+                UserName = "friend",
+                Id = "2813d05d-5e44-4af6-bcd4-b13ca4095834",
+                Position = new Position() { Longitude = 16.9, Latitude = 54.6, LastUpdate = DateTime.Now },
+
+            });
+            users.Add(new ApplicationUser()
+            {
+                UserName = "friend2",
+                Id = "4813d05d-5e44-4af6-bcd4-b13ca4095834",
+                Position = new Position() { Longitude = 16.9, Latitude = 54.6, LastUpdate = DateTime.Now },
+
+            });
+            users.Add(new ApplicationUser()
+            {
+                UserName = "friend3",
+                Id = "1813d05d-5e44-4af6-bcd4-b13ca4095834",
+                Position = new Position() { Longitude = 16.9, Latitude = 54.6, LastUpdate = DateTime.Now },
+
+            });
             var userFriends = userRepoMock.Object.FindAll();
             Assert.AreEqual(3, users.Count);
-
+            Assert.IsNotNull(userFriends);
         }
 
         [TestMethod()]
         public void FindLoggedFriendsOfTest()
         {
-            var user2 = new ApplicationUser();
+
             List<ApplicationUser> friends = new List<ApplicationUser>();
-            friends.Add(user2);
-            var user = new ApplicationUser() { Friends = friends, Position = new Position() { LastUpdate = DateTime.Now, Latitude = 52.1, Longitude = 52.1 } };
-            var userFriends = userRepoMock.Object.FindLoggedFriendsOf("239f4fae-ff76-4c80-b86c-b56666f4ac2e");
-            Assert.IsNotNull(userFriends);
-            Assert.Equals(friends, userFriends);
+            friends.Add(new ApplicationUser()
+            {
+                UserName = "friend",
+                Id = "2813d05d-5e44-4af6-bcd4-b13ca4095834",
+                Position = new Position() { Longitude = 16.9, Latitude = 54.6, LastUpdate = DateTime.Now },
+
+            });
+            userRepoMock.Setup(f => f.FindLoggedFriendsOf("239f4fae-ff76-4c80-b86c-b56666f4ac2e")).Returns(friends);
+            var result = userRepoMock.Object.FindLoggedFriendsOf("239f4fae-ff76-4c80-b86c-b56666f4ac2e");
+            Assert.IsNotNull(result);
+
         }
 
         [TestMethod()]
         public void FindFriendsOfTest()
         {
-            var user2 = new ApplicationUser();
             List<ApplicationUser> friends = new List<ApplicationUser>();
+            var user2 = new ApplicationUser()
+            {
+                UserName = "friend2",
+                Id = "2813d05d-5e44-4af6-bcd4-b13ca4095834",
+                Position = new Position() { Longitude = 16.9, Latitude = 54.6, LastUpdate = DateTime.Now },
+               
+            };
             friends.Add(user2);
-            var user = new ApplicationUser() { Friends = friends, Position = new Position() { LastUpdate = DateTime.Now, Latitude = 52.1, Longitude = 52.1 } };
-            var userFriends = userRepoMock.Object.FindFriendsOf("239f4fae-ff76-4c80-b86c-b56666f4ac2e");
-            Assert.IsNotNull(userFriends);
-            Assert.Equals(friends, userFriends);
+            userRepoMock.Setup(f => f.FindFriendsOf("8813d05d-5e44-4af6-bcd4-b13ca4095834")).Returns(friends);
+            var result = userRepoMock.Object.FindFriendsOf("8813d05d-5e44-4af6-bcd4-b13ca4095834");
+            Assert.AreEqual(friends, result);
+            Assert.IsNotNull(result);
+                     
         }
 
         [TestMethod()]
         public void FindByUsernameTest()
         {
-            var friends = new List<ApplicationUser>();
-            var user = new ApplicationUser() {Friends = friends, Position = new Position() {LastUpdate = DateTime.Now, Latitude = 52.1, Longitude = 52.1 } };
-            var mockedUsername = userRepoMock.Object.FindByUsername("mockedUsername");
-            Assert.IsNotNull(mockedUsername);
-            Assert.AreSame(user, mockedUsername);
+            var userName = "friend";
+            List<ApplicationUser> friends = new List<ApplicationUser>();
+           userRepoMock.Setup(f => f.FindByUsername("friend")).Returns(new ApplicationUser()
+            {
+                UserName = "friend",
+                Id = "8813d05d-5e44-4af6-bcd4-b13ca4095834",
+                Position = new Position() { Longitude = 16.9, Latitude = 54.6, LastUpdate = DateTime.Now },
+                Friends = friends
+            });
+            var result = userRepoMock.Object.FindByUsername("friend").UserName;
+            Assert.AreEqual(userName, result);
+           
+        }
+
+        public List<ApplicationUser> GetUserTest()
+        {
+            List<ApplicationUser> users = new List<ApplicationUser>();
+            users.Add(new ApplicationUser(){
+                UserName = "friend",
+                Id = "2813d05d-5e44-4af6-bcd4-b13ca4095834",
+                Position = new Position() { Longitude = 16.9, Latitude = 54.6, LastUpdate = DateTime.Now },
+               
+            });
+            users.Add(new ApplicationUser()
+            {
+                UserName = "friend2",
+                Id = "3813d05d-5e44-4af6-bcd4-b13ca4095834",
+                Position = new Position() { Longitude = 16.9, Latitude = 54.6, LastUpdate = DateTime.Now },
+
+            });
+            return users;
         }
     }
 }
